4d85bc7b47806deb1f638307e6f24671eb09d362
SatyaCMD <satyacmd.developer@gmail.com>
2025-12-07 13:17:51 +0530

    Update fix_bookings.py

--- fix_bookings.py
+++ fix_bookings.py
@@ -13,4 +13,3 @@ print(f"Deleted {result.deleted_count} bookings missing transaction_id.")
 
 result2 = repo.collection.delete_many({'passenger_details': {'$exists': False}})
 print(f"Deleted {result2.deleted_count} bookings missing passenger_details.")
-
